64102fb945fc854c47278d148d7eb847b91f3cce
Khanh Tran <ankhanhct@gmail.com>
2020-12-07 08:04:25 +0700

    F

--- server.js
+++ server.js
@@ -9,11 +9,9 @@ app.use(express.urlencoded({ extended: true }))
 
 const viewsDir = path.join(__dirname, 'views')
 app.use(express.static(viewsDir))
-app.use(express.static(path.join(__dirname, './public')))
-app.use(express.static(path.join(__dirname, '../images')))
-app.use(express.static(path.join(__dirname, '../media')))
-app.use(express.static(path.join(__dirname, '../../weights')))
-app.use(express.static(path.join(__dirname, '../../dist')))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'weights')))
+app.use(express.static(path.join(__dirname, 'dist')))
 
 app.set("view engine", "ejs");
 
